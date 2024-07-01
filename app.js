b87cd658b114ec500b0c11bbd2d001639b7bb3ba
Pico Sphere <155822841+picosphere0@users.noreply.github.com>
2024-07-01 10:57:15 +0330

    blob update 2

--- app.js
+++
@@ -1,8 +0,0 @@
-const blob = document.getElementsById("blob");
-
-document.body.onpointermove = event => {
-    const { clientX, clientY} = event;
-}
-
-blob.style.left = `${clientX}px`;
-blobl.style.top = `${clientY}px`;
